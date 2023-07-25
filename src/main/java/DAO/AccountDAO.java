c18f0aeb9a3fb326e8eb10ae7f1edcc55f09afb3
Jimmy Lin <whosejimmy@gmail.com>
2023-07-25 18:11:27 +0000

    DAOs added, ccount complete

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,63 @@
+package DAO;
+
+import java.sql.*;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    //Untested Class
+    private Account extractObject(ResultSet rs){
+        try{
+            if(!rs.next()){return null;}
+            int id = rs.getInt("account_id");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            return new Account(id, username, password);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public Account getAccountByUsername(String username){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "SELECT * FROM Account WHERE username=?";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, username);
+
+            ps.executeQuery();
+            ResultSet rs = ps.getGeneratedKeys();
+            return extractObject(rs);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    
+    public Account insertAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO Account (username, password) VALUES (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                int id = (int)rs.getLong(1);
+                return new Account(id, account.getUsername(), account.getPassword());
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
