0aed42c257bf74dc290b9115a09fb37fc64a2232
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-05 23:57:33 -0400

    PasswordUtils completed Untested

--- src/main/java/Model/UserModels/PasswordUtils.java
+++ src/main/java/Model/UserModels/PasswordUtils.java
@@ -1,5 +1,19 @@
 package Model.UserModels;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtils {
-    
+    private static BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(PasswordSecrets.saltRounds);
+    }
+
+    public static String encryptPassword(String password){
+        BCryptPasswordEncoder encoder = encoder();
+        return encoder.encode(password);
+    }
+
+    public static boolean matchPassword(String password, String encoded){
+        BCryptPasswordEncoder encoder = encoder();
+        return encoder.matches(password, encoded);
+    }
 }
