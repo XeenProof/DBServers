cb61c994039d9c41db8b112f507ce785f70668a1
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-10 19:39:09 -0400

    salt moved out

---
+++ src/main/java/Model/TodoList.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class TodoList {
+    private int id;
+    private String name;
+    private int owner_id;
+
+    public TodoList(){}
+    public TodoList(int id, String name, int owner_id){
+        this.id = id;
+        this.name = name;
+        this.owner_id = owner_id;
+    }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getOwner_id() {return owner_id;}
+    public void setOwner_id(int owner_id) {this.owner_id = owner_id;}
+}
